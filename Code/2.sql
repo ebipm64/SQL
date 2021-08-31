f0fde648a27b77024cf47cb10960c02a147b3e56
ebipm64 <71351619+ebipm64@users.noreply.github.com>
2021-08-31 11:36:04 -0500

    last

--- Code/2.sql
+++ Code/2.sql
@@ -1,3 +1,6 @@
-SELECT name,department,salary, 
-DENSE_RANK() OVER(PARTITION BY department ORDER BY salary DESC) salary_rank
-FROM test
+with cte as
+(select name,department,salary,
+ DENSE_RANK() OVER (PARTITION BY department ORDER BY salary desc) salary_rank
+from test) 
+select name,department,salary from cte
+where salary_rank =1
