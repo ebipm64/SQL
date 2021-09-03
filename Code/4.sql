4dc87838cb7b96dcae3b7ac39fac8dbae8dacbd7
ebipm64 <71351619+ebipm64@users.noreply.github.com>
2021-09-03 13:20:30 -0500

    third

--- Code/4.sql
+++ Code/4.sql
@@ -1,7 +1,18 @@
-SELECT customer_id,
-CASE 
-    WHEN (customer_id <= 100) THEN 'Premium'
-	WHEN (customer_id BETWEEN 101 AND 200) THEN 'PLUS'
-	ELSE 'Normal' 
-END AS class
-FROM customer
+WITH CTE AS(
+SELECT SUM(fare+extra+tax+tip) AS total_fare from taxi
+group by date
+order by date)
+SELECT 
+SUM(CASE 
+WHEN (total_fare BETWEEN 0.00 AND 4200000) THEN 1
+ELSE 0
+END) "REGULAR",
+SUM(CASE 
+WHEN (total_fare BETWEEN 4200001 AND 5500000) THEN 1
+ELSE 0
+END) "MEDUIM",
+SUM(CASE 
+WHEN (total_fare > 5500000) THEN 1
+ELSE 0
+END) "HIGH"
+FROM CTE
