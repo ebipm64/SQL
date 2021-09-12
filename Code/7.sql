65d35139ed7d03899fd6f153df98ac9c5c194c52
ebipm64 <71351619+ebipm64@users.noreply.github.com>
2021-09-12 14:42:51 -0500

    z

--- Code/7.sql
+++ Code/7.sql
@@ -1,7 +1,7 @@
-SELECT first_name,last_name,SUM(amount) as total_amount
+SELECT CONCAT(first_name, ' ',last_name) as customer_name,SUM(amount) as total_amount
 FROM payment
 INNER JOIN customer
 ON payment.customer_id = customer.customer_id
-GROUP BY first_name,last_name
-HAVING SUM(amount) < 150
-ORDER BY total_amount DESC
+GROUP BY CONCAT(first_name, ' ',last_name)
+HAVING SUM(amount) BETWEEN 120 AND 150
+ORDER BY SUM(amount) DESC
